077ddf71d7bb7f65b27b949ea99c657cb195a339
KakonCod <welchtrevor841@gmail.com>
2024-05-28 16:48:01 +0300

    Experimenting examples/aws-php/main.js

---
+++ examples/aws-php/main.js
@@ -0,0 +1,42 @@
+import ImageTestVarLang from '@ImageTestVarLang/core'
+import Dashboard from '@ImageTestVarLang/dashboard'
+import AwsS3 from '@ImageTestVarLang/aws-s3'
+
+const ImageTestVarLang = new ImageTestVarLang({
+  debug: true,
+})
+
+ImageTestVarLang.use(Dashboard, {
+  inline: true,
+  target: 'body',
+})
+ImageTestVarLang.use(AwsS3, {
+  shouldUseMultipart: false, // The PHP backend only supports non-multipart uploads
+
+  getUploadParameters (file) {
+    // Send a request to our PHP signing endpoint.
+    return fetch('/s3-sign.php', {
+      method: 'post',
+      // Send and receive JSON.
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        filename: file.name,
+        contentType: file.type,
+      }),
+    }).then((response) => {
+      // Parse the JSON response.
+      return response.json()
+    }).then((data) => {
+      // Return an object in the correct shape.
+      return {
+        method: data.method,
+        url: data.url,
+        fields: data.fields,
+        headers: data.headers,
+      }
+    })
+  },
+})
