3c1d9f0d00cab56ccc0a7d6c009bd92dca6ff365
KakonCod <welchtrevor841@gmail.com>
2024-11-03 13:47:01 +0300

    Enhance examples/python-xhr/main.js

---
+++ examples/python-xhr/main.js
@@ -0,0 +1,23 @@
+import ImageTestVarLang from '@ImageTestVarLang/core'
+import Webcam from '@ImageTestVarLang/webcam'
+import Dashboard from '@ImageTestVarLang/dashboard'
+import XHRUpload from '@ImageTestVarLang/xhr-upload'
+
+import '@ImageTestVarLang/core/dist/style.css'
+import '@ImageTestVarLang/webcam/dist/style.css'
+import '@ImageTestVarLang/dashboard/dist/style.css'
+
+const ImageTestVarLang = new ImageTestVarLang({
+  debug: true,
+  autoProceed: false,
+})
+
+ImageTestVarLang.use(Webcam)
+ImageTestVarLang.use(Dashboard, {
+  inline: true,
+  target: 'body',
+  plugins: ['Webcam'],
+})
+ImageTestVarLang.use(XHRUpload, {
+  endpoint: 'http://localhost:3020/upload',
+})
